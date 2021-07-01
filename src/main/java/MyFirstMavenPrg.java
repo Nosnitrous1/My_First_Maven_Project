102a3c463d0790780908e806e2bdb95c5c88e713
Nosnitrous1 <nosnitrous@yandex.ru>
2021-07-01 12:58:45 +0300

    Добавлены функции

--- src/main/java/MyFirstMavenPrg.java
+++ src/main/java/MyFirstMavenPrg.java
@@ -1,6 +1,25 @@
 public class MyFirstMavenPrg {
 
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+
+    hello("World");
+    hello("Vasia");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной "+ l + "=" + area(l));
+    double a = 3;
+    double b = 8;
+    System.out.println("Площадь прямоугольника "+ a + " "+ b + " = " + area(a,b));
   }
-}
+
+  private static double area(double len) {
+    return len * len;
+  }
+  private static double area(double s1, double s2) {
+    return s1 * s2;
+  }
+    private static void hello(String some) {
+    System.out.println("Hello " + some + "!");
+  }
+
+  }
