04636d249ca44f34b6ea65026a0148055e223b56
Nosnitrous1 <nosnitrous@yandex.ru>
2021-07-01 14:56:40 +0300

    Функции переехали в классы и стали методами

--- src/main/java/MyFirstMavenPrg.java
+++ src/main/java/MyFirstMavenPrg.java
@@ -5,19 +5,12 @@ public class MyFirstMavenPrg {
     hello("World");
     hello("Vasia");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной "+ l + "=" + area(l));
-    double a = 3;
-    double b = 8;
-    System.out.println("Площадь прямоугольника "+ a + " "+ b + " = " + area(a,b));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной "+ s.l + "=" + s.area());
+    Rectangle r = new Rectangle(3, 8);
+    System.out.println("Площадь прямоугольника "+ r.a + " "+ r.b + " = " + r.arear());
   }
 
-  private static double area(double len) {
-    return len * len;
-  }
-  private static double area(double s1, double s2) {
-    return s1 * s2;
-  }
     private static void hello(String some) {
     System.out.println("Hello " + some + "!");
   }
