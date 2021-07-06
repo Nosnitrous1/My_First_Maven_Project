c9b01526867be7642437d297a47b0ce94573fa60
Nosnitrous1 <46869145+Nosnitrous1@users.noreply.github.com>
2021-07-06 13:14:08 +0300

    Запуск Login теста через HTTP Session

---
+++ mantis-tests/src/test/java/tests/LoginTests.java
@@ -0,0 +1,17 @@
+package tests;
+
+import appmanager.HttpSession;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.io.IOException;
+import static org.testng.Assert.*;
+
+public class LoginTests extends TestBase {
+  @Test
+  public void testLogin() throws IOException {
+    HttpSession session = app.newSession();
+    assertTrue(session.login("administrator", "root"));
+    assertTrue(session.isLoggedInAs("administrator"));
+
+  }
+}
