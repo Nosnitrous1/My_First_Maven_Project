d7b1c5747fa7dc75bcafe1944d7c6de5054479b5
Nosnitrous1 <46869145+Nosnitrous1@users.noreply.github.com>
2021-07-07 12:26:49 +0300

    ftp не доделал

--- mantis-tests/src/test/java/tests/TestBase.java
+++ mantis-tests/src/test/java/tests/TestBase.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
+
 public class TestBase {
 
   protected static final ApplicationManager app
@@ -13,10 +15,13 @@ public class TestBase {
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
+    app.ftp().upload(new File("src/test/resources/config_default_inc.php"),
+            "config_default_inc", "config_default_inc.bak");
   }
 
   @AfterSuite
 public void tearDown() throws Exception {
+    app.ftp().restore("config_default_inc.bak", "config_default_inc.php");
     app.stop();
   }
 
